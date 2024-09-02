f3b5b5634cbc8d28434c7b58a42f08ff27b484b6
Sumit sahni <83834297+Sumit-Sahni@users.noreply.github.com>
2024-09-02 13:16:34 +0530

    Button Added

--- src/components/Button.stories.ts
+++ src/components/Button.stories.ts
@@ -1,31 +1,37 @@
-import React from 'react';
-import { Meta, StoryFn } from '@storybook/react';
-import Button from '../components/Button';
+// import React from 'react';
+// import { Meta, StoryFn } from '@storybook/react';
+// import Button from '../components/Button';
 
-export default {
-  title: 'Components/Button',
-  component: Button,
-   argTypes: {
-    onClick: { action: 'clicked' },
-  },
-} as Meta;
+// export default {
+//   title: 'Components/Button',
+//   component: Button,
+//   argTypes: {
+//     variant: {
+//       control: {
+//         type: 'select',
+//         options: ['first', 'second', 'third'],
+//       },
+//     },
+//     onClick: { action: 'clicked' },
+//   },
+// } as Meta<typeof Button>;
 
-const Template: StoryFn = (args) => <Button {...args} />;
+// const Template: StoryFn<typeof Button> = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
-Primary.args = {
-  variant: 'first',
-  children: 'first Button',
-};
+// export const Primary = Template.bind({});
+// Primary.args = {
+//   variant: 'first',
+//   children: 'First Button',
+// };
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  variant: 'second',
-  children: 'second Button',
-};
+// export const Secondary = Template.bind({});
+// Secondary.args = {
+//   variant: 'second',
+//   children: 'Second Button',
+// };
 
-export const Tertiary = Template.bind({});
-Tertiary.args = {
-  variant: 'third',
-  children: 'third button',
-};
+// export const Tertiary = Template.bind({});
+// Tertiary.args = {
+//   variant: 'third',
+//   children: 'Third Button',
+// };
